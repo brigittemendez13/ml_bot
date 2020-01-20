6c7bf69beaf273469a5a0d1e1e94fd51259a105b
mendez <mendezgalvezc@gmail.com>
2020-01-20 15:29:46 -0500

    php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+//backend
+/*backend*/
+$variable = 15;
+for($i = 0; $i < algo, $i++){
+
+};-
+while(){
+
+};
+do{
+
+}while();
+class ClassName
+{
+    function __construct(){
+
+    };
+    function hola(){
+
+    };
+}
+$var = 'fulano';
+$vector = ['fulano' => 15, 'como' => 20];
+echo $vector[$var];
+try{
+
+}catch($e){
+    echo $e;
+}
